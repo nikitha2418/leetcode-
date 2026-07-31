c73d188ce4861462f5b39a95ebaaca2824bf740c
Nikitha.S <saravana.rethanya@gmail.com>
2026-07-31 19:57:33 +0530

    [Syncode] GeeksForGeeks/Medium/Common Elements

---
+++ GeeksForGeeks/Medium/Common Elements/README.md
@@ -0,0 +1,13 @@
+# Common Elements
+
+## Difficulty: Medium
+
+## Platform: GeeksForGeeks
+
+## Problem Link
+[View Problem](https://www.geeksforgeeks.org/problems/common-elements5420/1?page=2&category=Java&sortBy=submissions)
+
+## Solved On
+31 Jul 2026 at 07:57 pm
+
+<h2><a href="https://www.geeksforgeeks.org/problems/common-elements5420/1?page=2&category=Java&sortBy=submissions">Common Elements</a></h2><h3>Difficulty Level: Medium</h3><hr>
---
+++ GeeksForGeeks/Medium/Common Elements/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static ArrayList<Integer> commonElements(int a[], int b[]) {
+        // Your code here
+       int i=0,j=0;
+       Arrays.sort(a);
+       Arrays.sort(b);
+         ArrayList<Integer> ans = new ArrayList<>();
+         
+         while(i<a.length && j<b.length){
+             if(a[i]==b[j]){
+                 ans.add(a[i]);
+                 i++;
+                 j++;
+             }
+             else if (a[i]<b[j]) i++;
+             else  j++;
+         }
+         return ans;
+    }
+}
